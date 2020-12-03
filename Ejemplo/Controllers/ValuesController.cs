a0f22b6afa11488883ec7c692c1803f05f385282
Carlos Vazquez <carlos.vazquez.unach@gmail.com>
2020-12-03 16:40:22 -0600

    Inicio Proyecto

---
+++ Ejemplo/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿namespace Ejemplo.Controllers
+{
+    using Ejemplo.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("Obtener")]
+        public IEnumerable<Persona> Get()
+        {
+            List<Persona> personas = new List<Persona> {
+                new Persona { Id = 1, Nombre = "Carlos", Paterno = "Vazquez", Materno = "Farrera"},
+                new Persona { Id = 2, Nombre = "Juan", Paterno = "Vazquez", Materno = "Farrera"},
+                new Persona { Id = 3, Nombre = "Hector", Paterno = "Vazquez", Materno = "Farrera"},
+                new Persona { Id = 4, Nombre = "Ariosto", Paterno = "Vazquez", Materno = "Farrera"},
+                new Persona { Id = 5, Nombre = "Eduardo", Paterno = "Vazquez", Materno = "Farrera"}
+            };
+
+            return personas;
+        }
+    }
+}
---
+++ Ejemplo/Models/Persona.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejemplo.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Paterno { get; set; }
+        public string Materno { get; set; }
+    }
+}
